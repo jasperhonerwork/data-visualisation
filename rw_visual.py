30d537f6f44c826239c6845dd33657d7bc29febf
jasper honer <jasperhoner.werk@outlook.com>
2021-10-07 22:49:21 +0200

    Better visuals and add 10x more walks.

--- rw_visual.py
+++ rw_visual.py
@@ -2,12 +2,28 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Make a random walk.
-rw = RandomWalk()
-rw.fill_walks()
+# Keep making new waks, as long as the program is active
+while True:
+    # Make a random walk.
+    rw = RandomWalk(50_000)
+    rw.fill_walks()
 
-# Plot the points in the walk
-plt.style.use('classic')
-fix, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    # Plot the points in the walk
+    plt.style.use('classic')
+    fix, ax = plt.subplots(figsize=(11, 6), dpi=128)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+
+    # Emphasize the first and last point.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100 )
+    
+    # Remove the axis.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
